0316e38a7d8616043082f9ad83e67290138dc386
miladaleali <miladaleali1392@gmail.com>
2024-03-10 10:59:22 +0000

    ADD: setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name="caissa_lab",
+        version="0.0.0",
+        author="Milad Aleali",
+        author_email="miladaleali1392@gmail.com",
+        packages=["caissa_lab"],
+        package_dir={"caissa_lab":"caissa_lab"},
+        url="http://github.com/miladaleali/caissa_lab/",
+        license="MIT",
+        install_requires=[]
+)
